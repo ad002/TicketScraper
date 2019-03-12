ca4f9d2d904dcbadeabf57415e99671ee14d62ce
ad002 <adrianhunold@gmx.de>
2019-03-12 16:19:26 +0100

    Redefined but mostly worked on the rotating IP file

--- scrape.py
+++ scrape.py
@@ -11,25 +11,29 @@ timeuntilfetch=900
 #Get the Time right now and add timeuntilfetch-time to it
 #nextfetchtime=
 
-def getData():
-    #html_doc = 'https://www.skyscanner.de/transport/fluge/colo/spu/190713/190721/?adults=2&children=0&adultsv2=2&childrenv2=&infants=0&cabinclass=economy&rtn=1&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false&ref=home#results'
-    #Fetch the content from URL
-    #page_response = requests.get(page_link, timeout = 10)
+#proxies= {"http": "http://"proxies, "https": "http://"proxies}
+#Alternativeley just delete / comment out this line and try with 
+#the format provided via my hideIP function (rewuests.get(proxies=proxies)
+#or format the output of hideIP in a line above 
+
+#Former this was a function but as I need proxies as global variable 
+#I just put it in the main programm
+#Calling the function get_proxies() I wrote in the other file to get a proxy
+
+
+
+def getData(proxy):
+   
+    #Getting the Website in HTML
+    website= requests.get("https://www.skyscanner.de/transport/fluge/cgn/spu/190713/190721/?adults=2&children=0&adultsv2=2&childrenv2=&infants=0&cabinclass=economy&rtn=1&preferdirects=true&outboundaltsenabled=false&inboundaltsenabled=false&ref=home#results", proxies=proxies)
+    #Open the Website's html and read it 
+    raw_html=open('website').read()
+    #html=BeautifulSoup(raw_html, 'html_parser')
     
-    #To use proxies,we can just use the fetched one we used 
-    #requests.get('https://example.org', proxies=proxies)
+    #Testing
+    print(html)
     
-    #Parse html
-    #page_content=BeautifulSoup(page_response.content, "html.parser")
-
-    #extract all html elements where price is stored
-    #prices= page_content.find_all(class_='CTASection__price-2bc7h price')
-   # if prices is None:
-   #     print("Can't get any price")
-   # print(f"The price is {prices} right now")
-
-#print(prices)
-
+        
 def userAgent():
     #Addition: User agent spoofing
     #Everytime you visit a website, it gets your browser information via user agent
@@ -47,21 +51,37 @@ def passData():
     abc=test
     #Don't forget to close the file
 
-def changeIP():
-   #Calling the function get_proxies() I wrote in the other file to get a proxy
-    hideIP.get_proxies()
-    print(proxies)
+
+#def convertProxieFormat(proxy):
+    #As rewuests.get needs proxy in a string format we have to do this typecasting
+    #converting proxy from 'set'-Datastructure to string
+    #proxy=str(proxy)
+    #Stripping of the brackets
+    #proxy=proxy.strip('{')
+    #proxy=proxy.strip('}')
+    #print(proxy)
+    #proxies= {
+    #"http": "http://"+proxy,
+    #"https": "https://"+proxy,}
+    #print(proxies)
     
-        
-            
-     
+
     
 
 def main():
 	#The real programm
+    print("Processing...")
+    proxy= hideIP.get_proxy()
+    print("Your IP adress is ", proxy)
+    convertProxieFormat(proxy)
+    #getData(proxy)
+    #getData()
 #	userAgent()
-#   getData()
-	changeIP()
 #	passData()
 #	timeout(timeuntilfetch)
 #   print(f"Next fetch will be in {timeuntilfetch/60} minutes at {nextfetchtime}")
+
+if __name__ == '__main__':
+  main()
+            
+     
